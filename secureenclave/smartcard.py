9633a56f9f2377571e319bdc596715bf75dd6b4b
Your Name <gonzaloab@gmail.com>
2025-05-18 18:47:58 -0400

    Bug fixes

--- secureenclave/smartcard.py
+++ secureenclave/smartcard.py
@@ -22,16 +22,18 @@ class SmartCard:
 
     def wait_for_it(self):
         state = None
+        card_not_found = False
         while True:
             _pids, new_state = scan_devices()
             if new_state != state:
                 devices = list_all_devices()
                 if devices:
                     logger.debug(devices)
-                    logger.debug('Card detected. Waiting 1 second for GPG to pick up the card.')
-                    time.sleep(1.0)
+                    if card_not_found:
+                        logger.debug('Card undetected. Waiting 1 second for GPG to pick up the card.')
+                        time.sleep(1.0)
                     return devices
-            state = new_state
+            card_not_found = True
             time.sleep(0.2)
 
     def list_cards(self):
